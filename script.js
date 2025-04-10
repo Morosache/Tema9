646fc6f0e043c9b84c2dabeeca309948f9637088
Elisei Morosan <elisei.morosan@emanuel.ro>
2025-04-10 11:00:14 +0300

    Am creat functia pentru initializarea boxurilor

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let canvasHeight = 0;
+let canvasWidth = 0;
 
 
 function setup() {
@@ -11,5 +13,32 @@ function draw() {
 }
 
 
+function initBoxes(width, height, cols, rows, matrix) {
+    let x = width;
+    let y = height;
+
+    for(let i = 0; i < rows; i++) {
+        const lines = [];
+
+        for(let j = 0; j < cols; j++) {
+            const box = {
+                x: x,
+                y: y, 
+                s: 40,
+                num: Math.floor(Math.random() * 10),
+                color: {
+                    r: Math.floor(Math.random() * 255),
+                    g: Math.floor(Math.random() * 255),
+                    b: Math.floor(Math.random() * 255),
+                },
+            };
+            lines.push(box);
+            x += 50;
+        }
+        matrix.push(lines);
+        x = width;
+        y += 50;
+    }
+}
 
 
