b65ed40a8e031dffd19197b0874ec2d44d737160
Elisei Morosan <elisei.morosan@emanuel.ro>
2025-04-10 10:59:10 +0300

    Am creat functia setup si draw din p5, si am dat dimensiunea canvasului, cat toata pagina

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function setup() {
+    canvasHeight = windowHeight;
+    canvasWidth = windowWidth;
+    createCanvas(canvasWidth, canvasHeight);
+}
+
+function draw() {
+    background("gray");
+}
+
+
+
+
